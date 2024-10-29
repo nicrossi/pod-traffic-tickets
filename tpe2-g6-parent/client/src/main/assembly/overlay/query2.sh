700f94fd766092e2707a572c5f3f7fdeef94888f
Matias Ricarte <mricarte@itba.edu.ar>
2024-10-28 21:24:54 -0300

    query scripts and update hazelcast version

---
+++ tpe2-g6-parent/client/src/main/assembly/overlay/query2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PATH_TO_CODE_BASE=`pwd`
+
+#JAVA_OPTS="-Djava.rmi.server.codebase=file://$PATH_TO_CODE_BASE/lib/jars/rmi-params-client-1.0-SNAPSHOT.jar"
+
+QUERY="2"
+MAIN_CLASS="ar.edu.itba.pod.tpe2.client.Client"
+
+java $JAVA_OPTS $* -Dquery=$QUERY -cp 'lib/jars/*'  $MAIN_CLASS
