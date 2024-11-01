9cf127fbfe3e646d65f5c5fe9296dec2380a2229
Nico Rossi <nrossi@itba.edu.ar>
2024-11-01 12:30:31 -0300

    Query3 implementation

---
+++ tpe2-g6-parent/api/src/main/java/ar/edu/itba/pod/tpe2/mapper/query3/Query3Mapper.java
@@ -0,0 +1,26 @@
+package ar.edu.itba.pod.tpe2.mapper.query3;
+
+import ar.edu.itba.pod.tpe2.common.Ticket;
+import com.hazelcast.mapreduce.Context;
+import com.hazelcast.mapreduce.Mapper;
+
+import java.time.LocalDateTime;
+
+public class Query3Mapper implements Mapper<String, Ticket, String, String> {
+    private static final String SEPARATOR = ":";
+    private final LocalDateTime from;
+    private final LocalDateTime to;
+
+    public Query3Mapper(LocalDateTime from, LocalDateTime to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public void map(String key, Ticket value, Context<String, String> context) {
+        if (value.getDate().isAfter(from) && value.getDate().isBefore(to)) {
+            String countyInfractionKey = String.format("%s:%s", value.getCounty(), value.getInfraction());
+            context.emit(countyInfractionKey, value.getPlate());
+        }
+    }
+}
---
+++ tpe2-g6-parent/client/src/main/java/ar/edu/itba/pod/tpe2/client/query/query3/Query3.java
@@ -0,0 +1,52 @@
+package ar.edu.itba.pod.tpe2.client.query.query3;
+
+import ar.edu.itba.pod.tpe2.client.query.QueryStrategy;
+import ar.edu.itba.pod.tpe2.client.util.DateUtils;
+import ar.edu.itba.pod.tpe2.collator.query3.Query3Collator;
+import ar.edu.itba.pod.tpe2.common.Ticket;
+import ar.edu.itba.pod.tpe2.mapper.query3.Query3Mapper;
+import ar.edu.itba.pod.tpe2.reducer.query3.Query3ReducerFactory;
+import com.hazelcast.core.ICompletableFuture;
+import com.hazelcast.mapreduce.Job;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.Validate;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+@NoArgsConstructor(force = true)
+public class Query3 implements QueryStrategy {
+    private final Map<String, String> args;
+
+    public Query3(Map<String, String> args) {
+        Validate.notNull(args, "Query 3, parameter 'args' cannot be null");
+        Validate.isTrue(args.containsKey("from"), "Query 3, parameter 'from' is required");
+        Validate.isTrue(args.containsKey("to"), "Query 3, parameter 'to' is required");
+        Validate.isTrue(args.containsKey("n"), "Query 3, parameter 'n' is required");
+        this.args = args;
+    }
+
+    @Override
+    public void run(Date timeStart, Job<String, Ticket> job) throws ExecutionException, InterruptedException {
+        LocalDateTime from = DateUtils.parseDate(args.get("from"));
+        LocalDateTime to = DateUtils.parseDate(args.get("to")).withHour(23).withMinute(59).withSecond(59);
+        int n = Integer.parseInt(args.get("n"));
+        ICompletableFuture<List<String>> future = job
+                .mapper(new Query3Mapper(from, to))
+                .reducer(new Query3ReducerFactory(n))
+                .submit(new Query3Collator());
+
+        List<String> results = future.get();
+        results.forEach(System.out::println);
+
+        Date timeEnd = new Date();
+        log.info("{} INFO [main] Client - Inicio del trabajo map/reduce", timeStart.toString());
+        log.info("{} INFO [main] Client - Fin del trabajo map/reduce", timeEnd.toString());
+
+    }
+}
